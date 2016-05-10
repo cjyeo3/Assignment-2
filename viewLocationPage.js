f14657557655838450141a92a418d986f1f82031
cjyeo3 <calvin_yjh@hotmail.com>
2016-05-10 12:23:11 +0800

    Test
    Test

--- viewLocationPage.js
+++ viewLocationPage.js
@@ -10,3 +10,4 @@ if (locationIndex !== null)
     // If a location name was specified, use it for header bar title.
     document.getElementById("headerBarTitle").textContent = locationNames[locationIndex];
 }
+ 
